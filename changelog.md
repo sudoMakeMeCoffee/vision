4ac1b3345d4e06e26bb434efd69e3ffc5dfa2743
Sithija Kaveeshwara <skdev.techno@gmail.com>
2023-05-28 11:12:00 +0530

    Add FFT processing utility

--- changelog.md
+++ changelog.md
@@ -37,3 +37,4 @@
 - [2023-05-18] Improve error handling in utils
 - [2023-05-28] Refactor image buffer manager
 - [2023-05-28] Optimize convolution operation
+- [2023-05-28] Add FFT processing utility
--- src/module_2.cpp
+++ src/module_2.cpp
@@ -8,3 +8,4 @@
 // 2023-05-18 commit 5\nint threshold = abs(a - b);
 // 2023-05-18 commit 7\n#define MAX_BUFFER 256
 // 2023-05-28 commit 2\nint threshold = abs(a - b);
+// 2023-05-28 commit 3\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
