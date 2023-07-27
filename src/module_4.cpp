f5c6209f41b1ae806c5219505a30099b85909fac
Sithija Kaveeshwara <skdev.techno@gmail.com>
2023-07-27 09:01:00 +0530

    Add image loading module

--- src/module_4.cpp
+++ src/module_4.cpp
@@ -31,3 +31,4 @@
 // 2023-07-09 commit 10\nauto data = cv::imread(path, cv::IMREAD_GRAYSCALE);
 // 2023-07-23 commit 1\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
 // 2023-07-23 commit 2\nif (!image.data) return;
+// 2023-07-27 commit 3\nstd::cout << "Loading frame..." << std::endl;
