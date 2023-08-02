1e24e3161041b7c08f7b423f1875b2b626f6d584
Sithija Kaveeshwara <skdev.techno@gmail.com>
2023-08-02 15:34:00 +0530

    Add unit tests for ImageProcessor

--- src/module_4.cpp
+++ src/module_4.cpp
@@ -34,3 +34,4 @@
 // 2023-07-27 commit 3\nstd::cout << "Loading frame..." << std::endl;
 // 2023-08-02 commit 1\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
 // 2023-08-02 commit 2\nclass ImageProcessor { public: void run(); };
+// 2023-08-02 commit 8\nusing namespace std;
