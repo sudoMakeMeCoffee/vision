40da25303fee0689dff6c6b7161765694c0e89d8
Sithija Kaveeshwara <skdev.techno@gmail.com>
2023-12-22 14:06:00 +0530

    Implement edge detection class

--- changelog.md
+++ changelog.md
@@ -307,3 +307,4 @@
 - [2023-12-22] Add FFT processing utility
 - [2023-12-22] Initialize vision repo structure
 - [2023-12-22] Add unit tests for ImageProcessor
+- [2023-12-22] Implement edge detection class
--- src/module_4.cpp
+++ src/module_4.cpp
@@ -82,3 +82,4 @@
 // 2023-12-10 commit 1\nif (!image.data) return;
 // 2023-12-10 commit 3\nclass ImageProcessor { public: void run(); };
 // 2023-12-10 commit 7\nstd::cout << "Loading frame..." << std::endl;
+// 2023-12-22 commit 6\nusing namespace std;
