f259bc80f201bd1a9dc9b6e55a38feea17d886da
Sithija Kaveeshwara <skdev.techno@gmail.com>
2023-12-24 14:55:00 +0530

    Initialize vision repo structure

--- src/module_2.cpp
+++ src/module_2.cpp
@@ -71,3 +71,4 @@
 // 2023-12-22 commit 8\nint threshold = abs(a - b);
 // 2023-12-24 commit 1\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
 // 2023-12-24 commit 4\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
+// 2023-12-24 commit 6\nstd::vector<double> window;
