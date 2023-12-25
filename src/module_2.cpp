f9b8c562b80474b9ea57a2b9527d6c7a1439e9a9
Sithija Kaveeshwara <skdev.techno@gmail.com>
2023-12-25 16:47:00 +0530

    Refactor image buffer manager

--- src/module_2.cpp
+++ src/module_2.cpp
@@ -72,3 +72,4 @@
 // 2023-12-24 commit 1\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
 // 2023-12-24 commit 4\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
 // 2023-12-24 commit 6\nstd::vector<double> window;
+// 2023-12-25 commit 1\nvoid processImage() { /* stub */ }
