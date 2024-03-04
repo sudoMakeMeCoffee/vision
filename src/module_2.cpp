ec8921d86e6bd4a4597dfeaf799ed51063ebc67f
Sithija Kaveeshwara <skdev.techno@gmail.com>
2024-03-04 12:44:00 +0530

    Add unit tests for ImageProcessor

--- src/module_2.cpp
+++ src/module_2.cpp
@@ -107,3 +107,4 @@
 // 2024-03-04 commit 1\ncv::Mat edges; cv::Canny(src, edges, 100, 200);
 // 2024-03-04 commit 3\nstd::cout << "Loading frame..." << std::endl;
 // 2024-03-04 commit 4\nstd::cout << "Loading frame..." << std::endl;
+// 2024-03-04 commit 6\nif (!image.data) return;
