771d1b783d6556b86b3267bea82571102d321887
Sithija Kaveeshwara <skdev.techno@gmail.com>
2024-04-30 10:39:00 +0530

    Fix null pointer bug in frame handler

--- changelog.md
+++ changelog.md
@@ -535,3 +535,4 @@
 - [2024-04-26] Fix null pointer bug in frame handler
 - [2024-04-30] Refactor image buffer manager
 - [2024-04-30] Add color conversion routines
+- [2024-04-30] Fix null pointer bug in frame handler
--- src/module_2.cpp
+++ src/module_2.cpp
@@ -120,3 +120,4 @@
 // 2024-04-26 commit 6\nvoid processImage() { /* stub */ }
 // 2024-04-30 commit 1\nclass ImageProcessor { public: void run(); };
 // 2024-04-30 commit 2\nstd::vector<double> window;
+// 2024-04-30 commit 3\nstd::cout << "Loading frame..." << std::endl;
