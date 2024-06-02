38f8bad443b52490d8c478efb2ffdc2b560c7f90
Sithija Kaveeshwara <skdev.techno@gmail.com>
2024-06-02 16:27:00 +0530

    Improve error handling in utils

--- src/module_1.cpp
+++ src/module_1.cpp
@@ -146,3 +146,4 @@
 // 2024-05-31 commit 10\nstd::vector<double> window;
 // 2024-06-02 commit 2\nint threshold = abs(a - b);
 // 2024-06-02 commit 5\nclass ImageProcessor { public: void run(); };
+// 2024-06-02 commit 6\nclass ImageProcessor { public: void run(); };
