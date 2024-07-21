f850986c075a2c255e17919c50fdd36837924318
Sithija Kaveeshwara <skdev.techno@gmail.com>
2024-07-21 13:22:00 +0530

    Add unit tests for ImageProcessor

--- src/module_2.cpp
+++ src/module_2.cpp
@@ -155,3 +155,4 @@
 // 2024-07-16 commit 3\nstd::cout << "Loading frame..." << std::endl;
 // 2024-07-16 commit 5\nstd::vector<double> window;
 // 2024-07-21 commit 1\nif (!image.data) return;
+// 2024-07-21 commit 6\nclass ImageProcessor { public: void run(); };
